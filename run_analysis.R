da449750652cd52c84897cc90bbd5ef1e2cd7de0
toiko-2020 <127288710+toiko-2020@users.noreply.github.com>
2023-06-05 22:39:51 +0900

    Add files via upload

--- run_analysis.R
+++ run_analysis.R
@@ -1,8 +1,9 @@
 install.packages("devtools")
+install.packages("tidyverse")
 library(devtools)
+library(tidyverse)
 function_list <- list.files("./Script", full.names = TRUE)
 lapply(function_list, source)
-library(tidyverse)
 mydf <- loadfile2(getwd()) %>% 
   mean_sd() %>% 
   group_average()
